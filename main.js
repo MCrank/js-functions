5e6e316a776e34f11e0ee3300c7f0c985e44a175
MCrank <marco_mcse@hotmail.com>
2018-08-25 10:26:39 -0500

    Object Shorthand Notation

--- main.js
+++ main.js
@@ -14,3 +14,17 @@ const firstName = 'Marco';
 const lastName = 'Crank';
 // greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+// Object shorthand notation
+let name = 'Marco';
+let age = '100000';
+
+// let person = {
+//   name: name,
+//   age: age
+// };
+
+// If variables are names the same as the key you can just use the var name
+let person = { name, age };
+
+console.log('Person:', person.name);
